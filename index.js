2f6625047ba7f34c45f777238aa1374d2bd7abef
Vince489 <vincentcornelius489@gmail.com>
2024-04-27 10:43:56 -0400

    ejs

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
     // Handle socket events here
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 server.listen(3000, () => {
